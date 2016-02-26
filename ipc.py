4920d577fc90a77c397e5689d91b9c1450030ee4
Russell <russelljarvis42@gmail.com>
2016-02-26 14:33:17 +1100

    project_clean_up

--- ipc.py
+++
@@ -1,5 +0,0 @@
-from IPython.parallel import Client
-c = Client(profile='mpi')
-view = c[:]
-view.activate() # enable magics
-view.run('initpar.py')
--- mpiipython.py
+++
@@ -1,9 +0,0 @@
-
-
-from IPython.parallel import Client
-c = Client()
-c.ids
-#c[:].apply_sync(lambda: "Hello, world!")
-view = c[:]
-view.activate()
-#view.run('memb_pot.py')
